1157d09b83cdc2381bff9d9bd833716f747eb39a
Andras Serfozo <subztep@gmail.com>
2018-07-06 06:12:27 +0100

    twitter test

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const server = new Hapi.Server({
 	port: PORT
 })
 
+let client = new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
+})
+
 server.route({
 	method: 'GET',
 	path: '/',
 	handler: (request, h) => {
-		return 'hello hapi '+process.env.consumer_key
+		//return 'hello hapi '+process.env.consumer_key
+
+		client.get('favorites/list', function (error, tweets, response) {
+            if (error) throw error;
+            //console.log(tweets);  // The favorites.
+            //console.log(response);  // Raw response object.
+            return response
+        })
 	}
 })
 
