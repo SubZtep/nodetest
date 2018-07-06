f8a2d59533840c5ff460c65bde786fc9a7bb32b1
Andras Serfozo <subztep@gmail.com>
2018-07-06 05:56:09 +0100

    hapi

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const express = require('express')
+/*const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
   .get('/', (req, res) => res.send('csao'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+const Hapi = require('hapi')
+const server = new Hapi.Server({
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return 'hello world'
+    }
+})
+
+async function start() {
+    try {
+        await server.start();
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
