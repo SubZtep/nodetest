3ce4e6efc0c60495f3380ddf5fb2f24e6193a74c
Andras Serfozo <subztep@gmail.com>
2018-07-07 02:58:17 +0100

    xx

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ const server = new Hapi.Server({
 // Init Twitter
 
 var T = new Twit({
-	consumer_key:         process.env.consumer_key,
+/* 	consumer_key:         process.env.consumer_key,
 	consumer_secret:      process.env.consumer_secret,
 	access_token:         process.env.access_token_key,
-	access_token_secret:  process.env.access_token_secret,
+  access_token_secret:  process.env.access_token_secret, */
+	consumer_key:         'a123',
+	consumer_secret:      'a123',
+	access_token:         'a1123',
+	access_token_secret:  'a123',
 	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 	strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
@@ -32,10 +36,10 @@ server.route({
 server.route({
 	method: 'GET',
 	path: '/tweets/{user}',
-	handler: (request, h) => {
-    T.get(
+	handler: async (request, h) => {
+    await T.get(
       'statuses/user_timeline',
-      {screen_name: request.params.name, count: 200},
+      {screen_name: request.params.name, count: 10},
       (err, data, response) => {
         console.log(data)
         return 'oki'
