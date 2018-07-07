907fcbf0840c867c76eefc549f59b96a98f2e5a8
Andras Serfozo <subztep@gmail.com>
2018-07-07 02:45:22 +0100

    Test

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
 const Hapi = require('hapi')
-//const Twitter = require('twitter')
-const axios = require('axios')
+const Vision = require('vision')
+const Twit = require('twit')
+const Pug = require('pug')
 
 const PORT = process.env.PORT || 5000
-
 const server = new Hapi.Server({
 	port: PORT
 })
 
-/*let client = new Twitter({
-	consumer_key: process.env.consumer_key,
-	consumer_secret: process.env.consumer_secret,
-	access_token_key: process.env.access_token_key,
-	access_token_secret: process.env.access_token_secret
-})*/
+
+// Init Twitter
+
+var T = new Twit({
+	consumer_key:         process.env.consumer_key,
+	consumer_secret:      process.env.consumer_secret,
+	access_token:         process.env.access_token_key,
+	access_token_secret:  process.env.access_token_secret,
+	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+	strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+
+// Routes
 
 server.route({
 	method: 'GET',
 	path: '/',
-	handler: async (request, h) => {
+	handler: (request, h) => h.view('index')
+})
 
-		let res = await axios.get('https://reqres.in/api/users')
-		return `<img src="${res.data.data[0].avatar}">`
-
-		/*let xxx = await client.get('favorites/list', (error, tweets, response) => {
-			if (error) {
-				console.log('error', error)
-			}
-			console.log(tweets);  // The favorites.
-		})
-
-		return xxx */
-
+server.route({
+	method: 'GET',
+	path: '/tweets/{user}',
+	handler: (request, h) => {
+    //console.log(request.params)
+    T = new Twit({})
+    T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+      console.log(data)
+    })
+		return 'yolo'
 	}
 })
 
-async function start() {
+
+// Start server
+
+const startup = async () => {
+	await server.register(Vision)
+	server.views({
+        engines: { pug: Pug },
+        relativeTo: __dirname,
+        path: 'public'
+	})
 	try {
-		await server.start();
+		await server.start()
+		console.log('Server running at:', server.info.uri)
 	} catch (err) {
 		console.log(err);
 		process.exit(1);
 	}
-	console.log('Server running at:', server.info.uri);
-};
-
-start();
+}
+startup()
